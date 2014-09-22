0cbb5febf016e66af1a47cf0a73c8e58b96d0beb
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-09-22 18:02:30 +0800

    Added constructor for Task.java

--- src/pact/Task.java
+++ src/pact/Task.java
@@ -6,6 +6,14 @@ public class Task {
 	public enum TASK_TYPE {
         FLOATING, DEADLINE, TIMED
     };
+    
+    public Task() {
+        taskName = "";
+        startTime = new Date();
+        endTime = new Date();
+        isCompleted = false;
+        isArchived = false;
+    }
 	
 	public String taskName;
 	public Date startTime;
