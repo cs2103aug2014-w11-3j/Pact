b704cd2a76526d7ef6f7c30a96428f34b9fc40d9
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-09-22 14:32:38 +0800

    Modify Task.java

--- src/pact/Task.java
+++ src/pact/Task.java
@@ -1,5 +1,17 @@
 package pact;
 
-public class Task {
+import java.util.Date;
 
-}
+public class Task {
+	public enum TASK_TYPE {
+        FLOATING, DEADLINE, TIMED
+    };
+	
+	public String taskName;
+	public Date startTime;
+	public Date endTime;
+	public TASK_TYPE type;		
+	public boolean isCompleted;
+	public boolean isArchived;
+};
+
