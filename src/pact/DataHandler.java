a79906b5919fbc48158552887af61278dde089d9
Jonathan Irvin Gunawan <jonathan.irvin@yahoo.com>
2014-09-22 15:05:13 +0800

    Added public methods of DataHandler.java

--- src/pact/DataHandler.java
+++ src/pact/DataHandler.java
@@ -1,5 +1,18 @@
 package pact;
 
-public class DataHandler {
+import java.util.ArrayList;
 
+public class DataHandler {
+	 public int addTask(Task taskToAdd) {
+		 return 0;
+	 }
+	 
+	 public int deleteTask(Task taskToDelete) {
+		 return 0;
+	 }
+	 
+	 public int searchTask(String keyword, ArrayList<Task> searchResult) {
+		 return 0;
+	 }
+	 
 }
