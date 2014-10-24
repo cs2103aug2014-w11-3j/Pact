b7c4d3108ebe3317d9816c159f6d0ab0e8ac64d5
Tamana Anna Tharakan <tamana293@gmail.com>
2014-10-25 00:49:24 +0800

    removed line in parserTest class

--- src/parser/ParserTest.java
+++ src/parser/ParserTest.java
@@ -11,17 +11,17 @@ public class ParserTest {
        
         
        //This is a equivalence test case for the invalid command partition
-        testCommand("invalid method name","Available commands : \"create\", \"update\", \"delete\", \"search\", \"display\", \"undo\" \n","put");
+        testCommandException("invalid method name","Available commands : \"create\", \"update\", \"delete\", \"search\", \"display\", \"undo\" \n","put");
         
         //This is a equivalence test case for invalid arguments partition for the create method
-        testCommand("test for invalid arguments",("Arguments required\n"+ "Format for Floating Task : add <taskName>\n"+"Format for Deadline Task : add <taskName> --en <endDate>\n" +"Format for Time Task : add <taskName> --st <startDate> --en <endDate>\n"),"create");
+        testCommandException("test for invalid arguments",("Arguments required\n"+ "Format for Floating Task : add <taskName>\n"+"Format for Deadline Task : add <taskName> --en <endDate>\n" +"Format for Time Task : add <taskName> --st <startDate> --en <endDate>\n"),"create");
         
         //This is test to check if an exception is thrown if the time or date format for start and end is different
-        testCommand("test for different date and time formats","start and end must have same format","create assignment 1 --start 22 Oct 2014 at 1000 --end 23/10/2014 at 2359");
+        testCommandException("test for different date and time formats","start and end must have same format","create assignment 1 --start 22 Oct 2014 at 1000 --end 23/10/2014 at 2359");
     
-        //
+        
     }
-    private void testCommand(String description,String expected,String command){
+    private void testCommandException(String description,String expected,String command){
         Parser parser = new Parser();
         try{
             parser.parse(command);
