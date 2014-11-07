7305f7e2e069a232f4960428bcc47ad9070498ee
Tamana Anna Tharakan <tamana293@gmail.com>
2014-11-07 21:17:47 +0800

    more system testing for PACT

--- src/pact/PactTest.java
+++ src/pact/PactTest.java
@@ -2,7 +2,7 @@ package pact;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import parser.Parser;
@@ -12,26 +12,36 @@ import utility.Keyword;
 public class PactTest {
     @Test
     public void test() {
-        //testCommand("add a task", "")
-        testCommand("display when text file is empty","Sorry, nothing found.", "display");
+       
+       
+        testCommand("add a task", "Task was added successfully.","create go for lunch");
+        testCommand("add a task","Task was added successfully.","create assignment --end 12112014");
+        testCommand("delete a task", "Task was deleted successfully.","delete go for lunch");
+        testCommand("update a task","Task was updated successfully.", "update assignment --name assignment 4");
+        testCommand("undo an action", "Undo was successful.","undo");
+        testCommand("add a task", "Task was added successfully.", "add homework --start 12112014 at 1200 --end 13112014 at 1200");
+        testCommand("clear the file","All tasks cleared successfully.","clear");
+        testCommand("display when text file is empty","There are no tasks in your task list", "display");
+        
     }
 
     private void testCommand(String description, String expected, String command) {
-        ArrayList<String> expectedA = new ArrayList<String>();
+        
         HashMap<Keyword, String> parsedCommand;
         ArrayList<String> result;
         Parser parser = new Parser();
         EventHandler eh = new EventHandler();
-        expectedA.add(expected);
+        
         
         try {
             parsedCommand = parser.parse(command);
             result = eh.determineCommand(parsedCommand);
-            assert(expectedA.equals(result));
+            assertEquals(description,expected,result.get(0));
+        
         }
         catch( Exception e) {
             
         }
-        expectedA.clear(); 
+       
     }
 }
